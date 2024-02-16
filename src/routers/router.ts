3891ee69249a6612b64d6884cfb8f6953a005339
Ilie Cristian Dorobat <idorobat@cisco.com>
2024-02-16 14:30:01 +0200

    Create calculation router path

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,5 +1,9 @@
 import {Request, Response, Router} from 'express';
-import {AREA, DATASET_TYPE, downloadDatasets} from './fetchUtils.js';
+import {AREA, DATASET_TYPE, downloadDatasets} from './fetch.utils.js';
+import {createRequire} from 'node:module';
+
+const require = createRequire(import.meta.url);
+const {JavaCaller} = require('java-caller');
 
 const router = Router();
 
@@ -8,10 +12,10 @@ router.get('', (req: Request, res: Response) => {
 });
 
 router.get('/dataset/fetch', async (req: Request, res: Response) => {
+    console.log('Starting downloading raw data...');
     const {datasetType = DATASET_TYPE.RAW, area = AREA.COUNTRIES} = req.query;
 
     try {
-        console.log('Starting downloading raw data...');
         await downloadDatasets(
             datasetType as DATASET_TYPE,
             area as AREA
@@ -21,6 +25,20 @@ router.get('/dataset/fetch', async (req: Request, res: Response) => {
     } catch (error) {
         res.status(500).send({error});
     }
+});
+
+router.get('/dataset/calculate', async (req: Request, res: Response) => {
+    console.log('Start data preparation...');
+    const java = new JavaCaller({
+        jar: 'libs/QoLI-Framework-1.2.jar',
+        mainClass: 'app.java.Main'
+    });
+    const {status, stdout, sterr} = await java.run(['--calculate=true', '--calculateIndicators=true']);
+    if (sterr) {
+        console.log('Data preparation has successfully finished!');
+        res.status(500).send({error: 'Execution failed'});
+    }
+    res.send('Calculation complete!');
 });
 
 export default router;
