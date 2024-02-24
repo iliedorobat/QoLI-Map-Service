55e41159cc3632bbc3159af635e7cccda131b023
Ilie Cristian Dorobat <idorobat@cisco.com>
2024-02-25 01:42:31 +0200

    Update app bootstrap mechanism

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import Express from 'express';
+import router from '#src/aggregator/stats.router.js';
 
 const app = Express();
 
 // Configuring Express to automatically parse the incoming JSON to an object
 app.use(Express.json());
+
+app.use(router);
 
 export default app;
