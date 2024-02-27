a4aed9c9374a762050a61f594e2ae96c83402a21
Ilie Cristian Dorobat <idorobat@cisco.com>
2024-02-27 02:01:12 +0200

    Allow cross-origin HTTP requests

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import Express from 'express';
+import cors from 'cors';
 import router from '#src/stats/stats.router.js';
 
 const app = Express();
 
 // Configuring Express to automatically parse the incoming JSON to an object
 app.use(Express.json());
+
+app.use(cors());
 
 app.use(router);
 
